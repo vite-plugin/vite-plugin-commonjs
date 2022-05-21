05a99525e069fc7556350bc2dcd47f69f889b086
younglei <youngleish@gmail.com>
2022-05-21 20:57:46 +0800

    feat: simpleWalk()

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,7 @@ import {
   multilineCommentsRE,
   singlelineCommentsRE,
 } from 'vite-plugin-utils'
+import { AcornNode } from './types'
 
 export function isCommonjs(code: string) {
   // Avoid matching the content of the comment
@@ -10,3 +11,28 @@ export function isCommonjs(code: string) {
     .replace(singlelineCommentsRE, '')
   return /\b(?:require|module|exports)\b/.test(code)
 }
+
+
+export function simpleWalk(
+  ast: AcornNode,
+  visitors: {
+    [type: string]: (node: AcornNode, ancestors: AcornNode[]) => void | Promise<void>,
+  },
+  ancestors: AcornNode[] = [],
+) {
+  if (!ast) return
+  if (Array.isArray(ast)) {
+    for (const element of ast as AcornNode[]) {
+      simpleWalk(element, visitors, ancestors)
+    }
+  } else {
+    ancestors = ancestors.concat(ast)
+    for (const key of Object.keys(ast)) {
+      (typeof ast[key] === 'object' &&
+        simpleWalk(ast[key], visitors, ancestors))
+    }
+  }
+  visitors[ast.type]?.(ast, ancestors)
+}
+// TODO
+simpleWalk.async = function simpleWalkAsync() { }
